82e1977d48bdbbcd959290d25bdce693db3fef8d
Ismael Celis <ismaelct@gmail.com>
2013-04-23 19:17:04 +0100

    POST and DELETE API

--- lib/api.rb
+++ lib/api.rb
@@ -9,4 +9,36 @@ class Api < Sinatra::Base
     MultiJson.dump Redirect.all
   end
   
+  post '/redirects' do
+    if payload['from'].nil? || payload['to'].nil?
+      invalid "JSON payload must have :from and :to URLs"
+    else
+      Redirect.set payload['from'], payload['to']
+      MultiJson.dump message: 'done'
+    end
+  end
+  
+  delete '/redirects' do
+    host = payload['from']
+    if host.nil?
+      invalid "JSON payload must include :from URL"
+    else
+      Redirect.del host
+      status 204
+      ''
+    end
+  end
+  
+  helpers do
+    
+    def payload
+      @payload ||= MultiJson.load(request.body.read)
+    end
+    
+    def invalid(msg)
+      status 422
+      MultiJson.dump message: msg
+    end
+  end
+  
 end
